5fb9fa0e08d4eeeff91561dd2a1a2fd0c1206545
ETR11 <90280938+ETR11@users.noreply.github.com>
2021-09-22 12:00:39 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+if (document.readyState !== "loading") {
+  initializeCode();
+} else {
+  document.addEventListener("DOMContentLoaded", function () {
+    initializeCode();
+  });
+}
+
+function initializeCode() {
+  document.getElementsByClassName("container").innerHTML = "<h1>Hello!</h1>";
+}
