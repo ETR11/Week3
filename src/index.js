6b5533347b2b3ac0f1939fce66f663dc5316e16a
ETR11 <90280938+ETR11@users.noreply.github.com>
2021-09-22 12:30:21 +0300

    Week3.11

--- src/index.js
+++ src/index.js
@@ -9,5 +9,28 @@ if (document.readyState !== "loading") {
 }
 
 function initializeCode() {
-  document.getElementsByClassName("container").innerHTML = "<h1>Hello!</h1>";
+  for (let i = 0; i < 5; i++) {
+    const container = document.getElementsByClassName("container");
+    let item = document.createElement("div");
+    item.className = "wiki-item";
+    let header = document.createElement("h1");
+    header.className = "wiki-header";
+    header.innerHTML = "Breed X";
+    let content = document.createElement("div");
+    content.className = "wiki-content";
+    let text = document.createElement("p");
+    text.className = "wiki-text";
+    text.innerHTML = (Math.random() + 1).toString(2).substring(2);
+    let imgCont = document.createElement("div");
+    imgCont.className = "img-container";
+    let img = document.createElement("img");
+    img.className = "wiki-img";
+    img.src = "";
+    container[0].appendChild(item);
+    item.appendChild(header);
+    item.appendChild(content);
+    content.appendChild(text);
+    content.appendChild(imgCont);
+    imgCont.appendChild(img);
+  }
 }
