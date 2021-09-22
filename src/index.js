ca377a47281ca0045556f6670b5a12b01fbe2c76
ETR11 <90280938+ETR11@users.noreply.github.com>
2021-09-22 19:10:12 +0300

    Week3.12

--- src/index.js
+++ src/index.js
@@ -2,20 +2,23 @@ import "./styles.css";
 
 if (document.readyState !== "loading") {
   initializeCode();
+  //dogPics();
 } else {
   document.addEventListener("DOMContentLoaded", function () {
     initializeCode();
+    //dogPics();
   });
 }
 
 function initializeCode() {
+  const breeds = ["husky", "shiba", "corgi", "terrier", "dachshund"];
   for (let i = 0; i < 5; i++) {
-    const container = document.getElementsByClassName("container");
+    const container = document.getElementById("dogs");
     let item = document.createElement("div");
     item.className = "wiki-item";
     let header = document.createElement("h1");
     header.className = "wiki-header";
-    header.innerHTML = "Breed X";
+    header.innerHTML = breeds[i];
     let content = document.createElement("div");
     content.className = "wiki-content";
     let text = document.createElement("p");
@@ -26,11 +29,26 @@ function initializeCode() {
     let img = document.createElement("img");
     img.className = "wiki-img";
     img.src = "";
-    container[0].appendChild(item);
+    dogPics(img, breeds[i]);
+    //img.src = "";
+    container.appendChild(item);
     item.appendChild(header);
     item.appendChild(content);
     content.appendChild(text);
     content.appendChild(imgCont);
     imgCont.appendChild(img);
   }
+}
+
+async function dogPics(img, dogo) {
+  await fetch("https://dog.ceo/api/breed/" + dogo + "/images/random")
+    .then((res) => res.json())
+    .then((info) => {
+      img.src = info.message;
+      console.log(info.message);
+    });
+  /*let response = await fetch("https://dog.ceo/api/breed/hound/images");
+  let dogos = await response.json();
+
+  console.log(dogos);*/
 }
